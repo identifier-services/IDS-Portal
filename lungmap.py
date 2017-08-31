48515e61c297ffefbc2abd324c767f380789be45
Andrew Magill <andrewbmagill@yahoo.com>
2017-08-30 22:43:07 -0400

    little cleaner

--- lungmap.py
+++ lungmap.py
@@ -6,30 +6,8 @@ def split_and_link(path):
     with open(path, 'r') as flat_file:
         reader = csv.DictReader(flat_file, delimiter=',')
         fieldnames = reader.fieldnames
-        unique_values = dict.fromkeys(fieldnames)
-        for k in unique_values.keys():
-            unique_values[k] = set()
-
-        # chunks are related to a specimen through SpecimenID
-        process_application_instances = []
-        # PAIs have probes and chunks as input, related through ProbeID and ... chunkid?
-        # ChunkID = SpecimenID + Slide + Section
-        # related to PAI through ... processID = probeid+chunkid
 
         elements = []
-
-        # specimen_fields = [
-        #     'Species',
-        #     'Age',
-        #     'SpecimenID',
-        #     'Strain',
-        #     'Genotype',
-        #     'Sex',
-        #     'EmbeddingSource',
-        #     'DissectionTime',
-        #     'PreperationMethod',
-        #     'TotalSets',
-        # ]
 
         specimen_fields = [
             'EmbeddingSource',
@@ -51,17 +29,6 @@ def split_and_link(path):
             'parents': []
         })
 
-        # pk = SpecimenID, no fk
-
-        # chunk_fields = [
-        #     'SpecimenID',
-        #     'SectionThickness',
-        #     'DirectionSection',
-        #     'Set',
-        #     'Slide',
-        #     'Section',
-        # ] # Relate to specimen (join with SpecimenID)
-
         chunk_fields = [
             'DirectionSection',
             'SectionThickness',
@@ -71,7 +38,6 @@ def split_and_link(path):
             'Section',
             'Slide',
             'Set',
-            # 'SpecimenID'
         ]
 
         elements.append({
@@ -79,17 +45,6 @@ def split_and_link(path):
             'fields': chunk_fields,
             'parents': ['specimen']
         })
-
-        # add a new field, ChunkID, fk = specimen__SpecimenID
-
-        # probe_fields = [
-        #     'GenSymbol',
-        #     'ProbeID',
-        #     'AccessionNumber',
-        #     'TemplateSequence',
-        #     'PrimerForward',
-        #     'PrimerReverse',
-        # ]
 
         probe_fields = [
             'AccessionNumber',
@@ -106,18 +61,8 @@ def split_and_link(path):
             'parents': []
         })
 
-        # pk = ProbeID
-
-        # process_fields = [
-        #     'Protocol',
-        #     'ProbeID',
-        #     'ChunkID?',
-        # ] # Relate to Probe and Chunk (usually 1:1 but one probe or chunk may have many processes)
-
         process_fields = [
             'Protocol',
-            i#'ProbeID', # probe__ProbeID
-            #'chunk__ChunkID',
         ]
 
         elements.append({
@@ -126,21 +71,10 @@ def split_and_link(path):
             'parents': ['probe','chunk']
         })
 
-        # add a field: 'ProcessID' as pk, fk: probe__ProbeID, chunk__SpecimenID -or- chunk__ChunkID
-
-        # image_fields = [
-        #     'URL',
-        #     'FileName',
-        #     'FilePath',
-        #     'Highlight',
-        # ] # Relate to Chunk through unique Process
-
         image_fields = [
             'URL',
             'FileName',
             'FilePath',
-            # 'chunk__ChunkID',
-            # 'ImageID',
         ]
 
         elements.append({
@@ -149,29 +83,9 @@ def split_and_link(path):
             'parents': ['chunk', 'image']
         })
 
-        # add ImageID as pk
-
-        # [(1, 'DirectionSection'), (1, 'EmbeddingSource'), (1, 'FilePath'), (1, 'Genotype'), (1, 'PreparationMethod'), (1, 'Protocol'), (1, 'SectionThickness'), (1, 'Species'), (1, 'Strain'), (1, 'TotalSets'), (2, 'Highlight'), (2, 'Sex'), (3, 'TissueType'), (4, 'Age'), (4, 'Rotate'), (4, 'Section'), (5, 'DissectionTime'), (5, 'Slide'), (8, 'Set'), (12, 'AccessionNumber'), (12, 'GeneSymbol'), (12, 'PrimerForward'), (12, 'PrimerReverse'), (12, 'ProbeID'), (12, 'TemplateSequence'), (16, 'SpecimenID'), (223, 'URL'), (238, 'FileName')]
-
-        for row in reader:
-            for k, v in row.items():
-                unique_values[k].add(v)
-
-        lengths = []
-
-        for k, v in unique_values.items():
-            lengths.append((len(v), k))
-
-        # print 'field count: %s' % len(lengths)
-
-        lengths.sort()
-        # print lengths
-
         unique_row_sets = dict.fromkeys(fieldnames)
         for k in unique_row_sets.keys():
             unique_row_sets[k] = []
-
-        
 
 if __name__ == '__main__':
     args = sys.argv
@@ -179,3 +93,33 @@ if __name__ == '__main__':
         print 'usage:\n$ python explode.py [filename]'
         sys.exit(0)
     split_and_link(args[1])
+
+## count of unique values, don't know if useful
+# DirectionSection 1
+# EmbeddingSource 1
+# FilePath 1
+# Genotype 1
+# PreparationMethod 1
+# Protocol 1
+# SectionThickness 1
+# Species 1
+# Strain 1
+#  TotalSets 1
+# Highlight 2
+# Sex 2
+# TissueType 3
+# Age 4
+# Rotate 4
+# Section 4
+# DissectionTime 5
+# Slide 5
+# Set 8
+# AccessionNumber 12
+# GeneSymbol 12
+# PrimerForward 12
+# PrimerReverse 12
+# ProbeID 12
+# TemplateSequence 12
+# SpecimenID 16
+# URL 223
+# FileName 238
