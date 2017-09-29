c6b6af8626cb6b28bd09e0dfcc56dbfd7104da2d
Bosworth <andrew.b.magill@gmail.com>
2017-09-29 16:27:44 -0500

    added staticfiles_dir to settings

--- project/settings.py
+++ project/settings.py
@@ -58,18 +58,6 @@ MIDDLEWARE = [
     'django.middleware.locale.LocaleMiddleware',
 ]
 
-xMIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'ids_auth.middleware.AgaveTokenRefreshMiddleware',
-]
-
 ROOT_URLCONF = 'project.urls'
 
 SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
@@ -142,9 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
